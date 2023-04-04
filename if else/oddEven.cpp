3ffe1f01f17e88b97f126566915fd11e36e47874
av1nDs0uza <avinalricdsouza@gmail.com>
2023-04-04 22:14:43 +0530

    if else started

---
+++ if else/oddEven.cpp
@@ -0,0 +1,13 @@
+// This is a if else program
+# include <iostream>
+using namespace std; 
+int main() {
+    int num; 
+    cout <<"Enter a number";
+    cin >> num;
+    if (num%2==0)
+    {
+        cout<<num <<" is even number";
+    }
+return 0;
+}

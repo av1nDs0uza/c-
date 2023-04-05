271ab6e94f145183ed1e97e1845f928082e94a7d
av1nDs0uza <avinalricdsouza@gmail.com>
2023-04-05 22:28:39 +0530

    if else

---
+++ if else/positveNegative.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int number;
+
+  number = -15;
+  if (number >= 0) {
+    cout << "You entered a positive integer: " << number << endl;
+  }
+  else {
+    cout << "You entered a negative integer: " << number << endl;
+  }
+
+  cout << "This line is always printed.";
+
+  return 0;
+}
